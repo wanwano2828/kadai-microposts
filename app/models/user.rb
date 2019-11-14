5591f8f4a9da465edff9bb8a15c6bda049a189d7
wanwano2828 <wanwano.2828@keio.jp>
2019-11-14 07:13:27 +0000

    favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favorite_microposts, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
@@ -29,5 +31,18 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def favorite(microposts)
+    self.favorites.find_or_create_by(micropost_id: microposts.id)
+  end
+  
+  def unfavorite(microposts)
+    favorite = self.favorites.find_by(micropost_id: microposts.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(microposts)
+    self.favorite_microposts.include?(microposts)
   end
 end
